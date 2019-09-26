4afbfc0d59d257726904509039b2d902ee43b508
Alex Montiel <ale.pedrero18@gmail.com>
2019-09-25 21:57:06 -0500

    2nd commit se agrega el llenado de matriz con letras aleatorias del alfabeto.

---
+++ your-project/sopa-de-letras.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+
+dicc_palabras = {'animal': ['cobaya', 'gato', 'conejo', 'huron', 'tigre', 'tortuga', 'camaron', 'aguila', 'lobo', 'gusano'],
+                 'capital': ['paris', 'ontario', 'bolivia', 'madrid', 'brasilia', 'tokio', 'praga', 'singapur', 'moscu', 'berlin'],
+                 'ironhack': ['ironhack', 'data', 'java', 'analisis', 'jupyter', 'pycharm', 'mongodb', 'python', '']}
+alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+def nombre(jugador):
+    jugador = input('Nombre Jugador:')
+    return 'Bienvenido ' + jugador + '¡Prepárate para jugar:'
+
+
+matriz2 = np.ones((20, 20))
+#print(matriz2)
+
+matrizjuego = [None] * 10
+for i in range(0,10):
+    matrizjuego[i] = [None] * 10
+
+for i in range(0,10):
+    for j in range(0,10):
+        matrizjuego[i][j] = alfabeto[random.randint(0,26)]
+
+matrizjuego = np.array(matrizjuego)
+print(matrizjuego)
