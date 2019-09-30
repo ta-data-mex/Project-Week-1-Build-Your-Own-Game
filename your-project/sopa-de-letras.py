377eb9d0d1d7ed17b87457900e7e519cdf52576b
Alex Montiel <ale.pedrero18@gmail.com>
2019-09-30 15:30:42 -0500

    commit x

--- your-project/sopa-de-letras.py
+++ your-project/sopa-de-letras.py
@@ -1,91 +1,91 @@
 import numpy as np
 import random
 
-# Diccionario de palabras a utilizar para llenar la sopa de letras
-dicc_palabras = {'animal': ['cobaya', 'gato', 'conejo', 'huron', 'tigre', 'tortuga', 'camaron', 'aguila', 'lobo',
-                            'gusano'],
-                 'capital': ['paris', 'ontario', 'bolivia', 'madrid', 'brasilia', 'tokio', 'praga', 'singapur',
-                             'moscu', 'berlin'],
-                 'ironhack': ['ironhack', 'data', 'github', 'analisis', 'jupyter', 'pycharm', 'mongodb', 'python',
-                              'alex', 'ramiro']}
-alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u',
+dicc_palabras = {'animal': ['cobaya', 'gato', 'conejo', 'huron', 'tigre'],  # Diccionario para llenar la sopa de letras
+                 'capital': ['paris', 'ontario', 'berlin', 'madrid', 'brasilia'],
+                 'ironhack': ['codigo', 'data', 'github', 'analisis', 'jupyter', ]}
+alfa = ['b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'u',
         'v', 'w', 'x', 'y', 'z']  # para rellenar la matriz
 
 
-encontradas = [] # Lista que guarda las palabras encontradas.
+def elegir_tema_sopa():  # Escoge un tema del diccionario de palabras
+    switch = True  # Para control del while
+    while switch:  # While para validar hasta que la opcion sea correcta;
+        tema = input("Elige un tema para jugar entre 'animal' 'capital' o 'ironhack': ").lower()
+        if tema == 'animal' or tema == 'capital' or tema == 'ironhack':
+            switch = False
+            return dicc_palabras[tema]  # Retorna las palabras del tema en el diccionario
+        else:
+            print("'Opcion inválida, elige un tema entre 'animal', 'capital' o 'ironhack' ")
+            continue
 
 
-def elegir_tema_sopa(tema):
-    tema = dicc_palabras[tema]
-    return tema
-
-
-# print(elegir_tema_sopa('animal'))
-# print(elegir_tema_sopa('capital'))
-# print(elegir_tema_sopa('ironhack'))
-
-# return [['c', 'o', 'b', 'a', 'y', 'a'], ['g', 'a', 't', 'o'], ['c', 'o', 'n', 'e', 'j', 'o'],
-def descomponer_array_tema(tema):
-    tema = elegir_tema_sopa(tema) #lista de palabras del diccionario
+def descomponer_array_tema():
+    tema_list = elegir_tema_sopa() #lista de palabras del diccionario
     temp = []
-    for item in tema:
-        temp.append([i for i in item])
-    print(temp)
+    for item in tema_list:
+        temp.append([i for i in item])  # Descompone cada palabra en caracteres y guarda cada palabra partida en lista
+    matriztemp = np.array(temp) #  Convierte la lista con la palabra partida en un arreglo NUMPY
 
-    return temp
-
-
-print('final: ', descomponer_array_tema('animal'))
+    return tema_list, temp  # Devuelve el arreglo de palabras descompuesta por caracter y la lista original
 
 
-def nombre(jugador):
-    jugador = input('Nombre Jugador:')
-    return 'Bienvenido ' + jugador + '¡Prepárate para jugar:'
-
-
-def crearMatriz(num):
+def crearMatriz(num):  # Funcion que define el tamaño de la matriz de juego y la inicializa con letras aleatorias
     n = num
     matrizjuego = [None] * n
     for i in range(len(matrizjuego)):
         matrizjuego[i] = [None] * n
 
     for i in range(0,n):
-        for j in range(0,n):
-            matrizjuego[i][j] = alfa[random.randint(0,26)]
-    matrizjuego = np.array(matrizjuego)
-    return matrizjuego
+        for j in range(0, n):
+            matrizjuego[i][j] = alfa[random.randint(0, 23)]  # Llenado desde la lista 'alfa'
+    matrizjuego = np.array(matrizjuego)  # Convierte la lista en un arreglo Numpy
+    return matrizjuego  # Devuelve una matriz NUMPY de nxn con letras del alfabeto aleatoriamente
 
 
-def info_pos_actual(matrix,nxn,esFila,pos):
-    #Muestra y guarda la información de la posición del arreglo en la que se encuentra en una lista.
-    temp_valores = []
-    temp_espacios = 0
+def acomodar():
+    switch = True
+    while switch:
+        lista, tema = descomponer_array_tema()
+        sopa = crearMatriz(18)
+        int_rndm = random.randint(2,8)
+        int_rndm2 = random.randint(1,3)
 
-    for i in range(nxn):
-        if esFila:
-            if matrix[pos][i] != '':  #Es fila
-                temp_valores.append(temp_espacios)
-                temp_valores.append(matrix[pos][i])
-            else:
-                temp_espacios += 1
-        else:
-            if matrix[i][pos] != '':  #Es columna
-                temp_valores.append(temp_espacios)
-                temp_valores.append(matrix[i][pos])
-                temp_espacios = 0
-            else:
-                temp_espacios += 1
-    if temp_espacios == nxn:
-        temp_valores.append(temp_espacios)
-        temp_valores.append("0")
-    return temp_valores
+        for x in range(len(tema)):
+            for y in range(len(tema[x])):
+                sopa[x*int_rndm2][y+int_rndm] = tema[x][y]
+                switch = False
+    numpysopa = np.array(sopa)
+    return numpysopa, lista  # Regresa la sopa de letra con las palabras a buscar ya acomodadas en el array
 
 
+def palabras_encontradas():
+    sopa, lista = acomodar()
+    palabras = []  # Almacena las palabras encontradas.
+    encontradas = 0
+    faltantes = len(lista)
+    print(sopa)
 
+    while len(palabras) < len(lista):
+        #global encontradas, faltantes
+        intento = input("Escribe la palabra encontrada: ").lower()
+        if intento in lista and intento not in palabras:
+            encontradas += 1
+            faltantes -= 1
+            palabras.append(intento)
+        print('Palabra encontrada: ', palabras)
+        print(f'Encontraste: {encontradas}, faltan {faltantes} palabras')
+    return "¡JUEGO TERMINADO!"
 
-
-
-print(crearMatriz(18))
-
-#def acomodar_palabra(nxn,matrix,palabras):
-
+gameOn = True
+while gameOn:
+    respuesta = input("¿Desea jugar una partida de Sopa de Letras? Simon/nelprro: ").lower()
+    if respuesta == 'simon':
+        palabras_encontradas()
+        print("--------------------------------")
+        print("¡JUEGO TERMINADO!")
+        print("¡ENCONTRASTE TODAS LAS PALABRAS!")
+        print("--------------------------------")
+    elif respuesta == 'nelprro':
+        print("Bye perro")
+        gameOn = False
